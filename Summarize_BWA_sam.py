5c5409ef5d9ed5c5bc26c3bf04159b2456e658fa
kunlin0814 <abc730814@gmail.com>
2020-04-15 16:47:00 -0400

    Update Summarize_BWA_sam.py

--- Summarize_BWA_sam.py
+++ Summarize_BWA_sam.py
@@ -54,10 +54,10 @@ Onemap_rate = float(Onemapped)/total
 incorrect_rate = float(incorrect)/total
 unmapped_rate = float(unmapped)/total
 
-#summary.write(output_name + '\n')
-#summary.write('ID\tTotal_pairs\tUniquely_mapped_rate\tRepeatedly_mapped_rate\t1read_mapped_rate\tIncorrectly_mapped_rate\tUnmapped_rate\tUniquely_mapped\tRepeatedly_mapped\t1read_mapped\tIncorrectly_mapped\tUnmapped\tTotal_reads\n')
-#summary.write(output_name+'\t'+str(unique) + '\t' + str(duplicate) + '\t' + str(Onemapped) + '\t' + str(incorrect) + '\t' + str(unmapped) + '\t' + str(total) + '\t' + str(pairs) + '\n')
-#summary.write(output_name+'\t'+str(pairs)+ '\t'+ str(unique_rate) + '\t' + str(dup_rate) + '\t' + str(Onemap_rate) + '\t' + str(incorrect_rate) + '\t' + str(unmapped_rate) +'\t'+str(unique)+'\t' + str(duplicate) + '\t' + str(Onemapped) + '\t' + str(incorrect) + '\t' + str(unmapped) + '\t' + str(total) + '\t\t\n')
+summary.write(output_name + '\n')
+summary.write('ID\tTotal_pairs\tUniquely_mapped_rate\tRepeatedly_mapped_rate\t1read_mapped_rate\tIncorrectly_mapped_rate\tUnmapped_rate\tUniquely_mapped\tRepeatedly_mapped\t1read_mapped\tIncorrectly_mapped\tUnmapped\tTotal_reads\n')
+summary.write(output_name+'\t'+str(unique) + '\t' + str(duplicate) + '\t' + str(Onemapped) + '\t' + str(incorrect) + '\t' + str(unmapped) + '\t' + str(total) + '\t' + str(pairs) + '\n')
+summary.write(output_name+'\t'+str(pairs)+ '\t'+ str(unique_rate) + '\t' + str(dup_rate) + '\t' + str(Onemap_rate) + '\t' + str(incorrect_rate) + '\t' + str(unmapped_rate) +'\t'+str(unique)+'\t' + str(duplicate) + '\t' + str(Onemapped) + '\t' + str(incorrect) + '\t' + str(unmapped) + '\t' + str(total) + '\t\n')
 
 
 #summary.close()
