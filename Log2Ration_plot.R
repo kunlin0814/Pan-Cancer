1743fa559f381d8825515c791ee08e54c46a2469
kunlin0814 <abc730814@gmail.com>
2020-08-20 16:15:08 -0400

    Update Log2Ration_plot.R

--- Log2Ration_plot.R
+++ Log2Ration_plot.R
@@ -6,12 +6,15 @@ library(gridExtra)
 file <- fread("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/PanCancerCoverage/MC_Coverage/CMT-13_CoverageLog2Ratio.txt",
               header = F)
 chrom_pos <- strsplit(file$V1,split = ":")
-newLable <- c()
-for (i in 1:length(file$V1)){
-  newLable <- c(newLable,chrom_pos[[i]][1])
 
+categorize <- function(x){
+ data <- strsplit(x,split = ":")
+ return (data[[1]][1])
 }
-file$V3 <- newLable
+
+chromCategory <- as.vector(sapply(file$V1,FUN =categorize ))
+
+file$V3 <- chromCategory
 chrom <- c()
 for (i in 1:38){
   chrom <- c(chrom,paste("chr",i,sep = ""))
@@ -20,30 +23,30 @@ chrom <- c(chrom, "chrX")
 
 regular.text <- element_text(colour="black",size=20);
 
-pdf("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/PanCancerCoverage/MC_Coverage/V3MC_Log2Ratio.plot.pdf"
+pdf("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/PanCancerCoverage/MC_Coverage/V6MC_Log2Ratio.plot.pdf"
     , height=4.98, width=6.5);
+testData <- file[1:10000]
 
-
-#factor(file$V1, levels = file$V1)
-for (each in chrom){
-eachChrom <- file[V3==each,]
-
-p <- ggplot(data=eachChrom, aes(x=factor(eachChrom$V1, levels = eachChrom$V1), y=V2)) +
-  geom_bar(stat="identity", fill="steelblue")+
+p <- ggplot(data=file, aes(x=factor(file$V1, levels = file$V1), y=V2, fill = V3)) +
+  geom_bar(stat="identity")+
   xlab("Chrom Position")+
   ylab("log2 Ratio (Tumor/Normal)")+
-  ggtitle(each)+
+  ggtitle("CMT-2")+
   theme_minimal()+
   theme(
+    legend.title= element_blank(), 
+    legend.position="top", 
+    legend.text=element_text(size = 10), 
     title = regular.text,
     axis.title.x = regular.text,
     axis.title.y = regular.text,
     axis.text.x = element_blank(),
     axis.text.y = regular.text,
-    panel.background=element_blank(), 
+    panel.background=element_blank(),
+    legend.key.size = unit(0.25,"line"),
     axis.line=element_line(color="black"))
 print(p)
-}
+
 
 dev.off()
 
