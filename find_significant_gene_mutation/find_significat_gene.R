d3114740f4ee64fb82cd4d988cb93a0a2f6fcb57
kunlin0814 <42413453+kunlin0814@users.noreply.github.com>
2021-01-24 21:52:33 -0500

    Update find_significat_gene.R

--- find_significant_gene_mutation/find_significat_gene.R
+++ find_significant_gene_mutation/find_significat_gene.R
@@ -6,30 +6,17 @@ base_dir <- "/Volumes/Research/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/
   #"G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/VAF/Burair_filtering3/Mutect1"
 seperator <- "/"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 retro_gene_list <- fread("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Retro_gene_finding/RetroGeneList/new_retro_gene_list_CanFam3.1.99gtf.txt",
                          header = F)
 
 whole_wes_table <- fread("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/arrange_table/whole_wes_table.txt") 
 exclude <- unique(unlist(whole_wes_table[The_reason_to_exclude!="Pass QC",.(Case_ID)]))
-=======
-=======
->>>>>>> parent of f1292fb... Update find_significat_gene.R
-=======
->>>>>>> parent of f1292fb... Update find_significat_gene.R
-=======
->>>>>>> parent of f1292fb... Update find_significat_gene.R
-=======
->>>>>>> parent of f1292fb... Update find_significat_gene.R
+
 variant <- fread(paste(base_dir,"significant","variant_samplewise_p_value_total_final_Filtering3_VAF_Mutect_orientBias3.gz",sep = seperator))
 gene <- fread(paste(base_dir,"significant","gene_samplewise_p_value_total_final_Filtering3_VAF_Mutect_orientBias3.gz",sep = seperator))
 # why dup?
->>>>>>> parent of f1292fb... Update find_significat_gene.R
+
 
 ## check duplicated
 # a <- gene[sample_names=="CCB040105"& ensembl_id=="ENSCAFG00000001781"]
@@ -50,8 +37,6 @@ gene <- fread(paste(base_dir,"significant","gene_samplewise_p_value_total_final_
 #                          sheet ="WES_WGS")
 # 
 # Breed_info <- setDT(Breed_info)
-<<<<<<< HEAD
-=======
 sample_variant <- fread(paste(base_dir,"significant","variant_samplewise_p_value_total_final_Filtering3_VAF_Mutect_orientBias3.gz",sep = seperator))
 sample_gene <- fread(paste(base_dir,"significant","gene_samplewise_p_value_total_final_Filtering3_VAF_Mutect_orientBias3.gz",sep = seperator))
 
@@ -62,26 +47,16 @@ Breed_info <- read_excel("G:/MAC_Research_Data/Pan_cancer/Pan-Cancer-Manuscript/
                          sheet ="WES_WGS")
 
 Breed_info <- setDT(Breed_info)
->>>>>>> parent of 37bf719... Update find_significat_gene.R
+
 mutect_after_vaf <- fread(paste(base_dir,"total_final_Filtering3_VAF_Mutect_withBreeds_orientBiasShaying.gz",sep =seperator))
 #mutect_after_vaf <- mutect_after_vaf[,chrom_loc:=paste(chrom,pos,sep = "_")]
 total_sample <- unique(mutect_after_vaf$sample_names)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 mutect_after_vaf <- fread(paste(base_dir,"total_final_Filtering3_VAF_Mutect_withBreeds_orientBiasShaying.gz",sep =seperator))
 #mutect_after_vaf <- mutect_after_vaf[,chrom_loc:=paste(chrom,pos,sep = "_")]
 total_sample <- unique(mutect_after_vaf$sample_names)
 
->>>>>>> parent of f1292fb... Update find_significat_gene.R
-=======
->>>>>>> parent of f1292fb... Update find_significat_gene.R
-=======
->>>>>>> parent of f1292fb... Update find_significat_gene.R
-=======
->>>>>>> parent of f1292fb... Update find_significat_gene.R
 # breed <- sapply(total_sample,FUN = match_table, column="Breeds",table=Breed_info)
 # mutect_after_vaf$Breeds <- breed
 
@@ -119,7 +94,6 @@ for (index in 1:length(total_sample)) {
   sample <- total_sample[index]
   info_sum <- NULL
   variant_loc <- mutect_after_vaf[sample_names==sample,.(ensembl_id)]
-<<<<<<< HEAD
 
   each_sample_ensmbl_id <- sort(unique(variant_loc[["ensembl_id"]]))
   for (i in each_sample_ensmbl_id ){
@@ -128,10 +102,6 @@ for (index in 1:length(total_sample)) {
   for (i in sort(unique(variant_loc[["ensembl_id"]])) ){
     info <- mutect_after_vaf[sample_names==sample & ensembl_id==i,]
 
-=======
-  for (i in sort(unique(variant_loc[["ensembl_id"]])) ){
-    info <- mutect_after_vaf[sample_names==sample & ensembl_id==i,]
->>>>>>> parent of 37bf719... Update find_significat_gene.R
     target <- mutect_after_vaf[sample_names==sample & ensembl_id==i, .(tRef,tAlt)]
     target_combine <- target[, .(tRef = sum(tRef), tAlt = sum(tAlt)),]
     others <- mutect_after_vaf[sample_names==sample & ensembl_id!=i, .(tRef,tAlt)]
@@ -206,7 +176,6 @@ for (index in 1:length(tumor_type)) {
   tumor <- tumor_type[index]
   info_sum <- NULL
   variant_loc <- mutect_after_vaf[tumor_type==tumor,.(ensembl_id)]
-<<<<<<< HEAD
   each_sample_ensmbl_id <- sort(unique(variant_loc[["ensembl_id"]]))
   for (i in each_sample_ensmbl_id) {
 
@@ -214,10 +183,6 @@ for (index in 1:length(tumor_type)) {
     ensembl_id <- i
 
     gene_name <- mutect_after_vaf[tumor_type==tumor & ensembl_id==i, .(gene_name)]$gene_name[1]
-=======
-  for (i in sort(unique(variant_loc[["ensembl_id"]])) ){
-    info <- mutect_after_vaf[tumor_type==tumor & ensembl_id==i,]
->>>>>>> parent of 37bf719... Update find_significat_gene.R
     target <- mutect_after_vaf[tumor_type==tumor & ensembl_id==i, .(tRef,tAlt)]
     target_combine <- target[, .(tRef = sum(tRef), tAlt = sum(tAlt)),]
     others <- mutect_after_vaf[tumor_type==tumor & ensembl_id!=i, .(tRef,tAlt)]
