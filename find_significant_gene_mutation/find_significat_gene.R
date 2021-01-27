c0bf6750c24708656d539cc8655b335f665f63e9
kunlin0814 <42413453+kunlin0814@users.noreply.github.com>
2021-01-26 23:55:44 -0500

    Update find_significat_gene.R

--- find_significant_gene_mutation/find_significat_gene.R
+++ find_significant_gene_mutation/find_significat_gene.R
@@ -1,16 +1,18 @@
 library(data.table)
 library(tidyverse)
 library(readxl)
-source("C:/Users/abc73/Documents/GitHub/R_util/my_util.R")
-  #"/Volumes/Research/GitHub/R_util/my_util.R")
-base_dir <- #"/Volumes/Research/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/VAF/Burair_filtering3/Mutect1"
-  "G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/VAF/Burair_filtering3/Mutect1"
+source(#"C:/Users/abc73/Documents/GitHub/R_util/my_util.R")
+  "/Volumes/Research/GitHub/R_util/my_util.R")
+base_dir <- "/Volumes/Research/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/VAF/Burair_filtering3/Mutect1"
+  #"G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/VAF/Burair_filtering3/Mutect1"
 seperator <- "/"
 
-retro_gene_list <- fread("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Retro_gene_finding/RetroGeneList/new_retro_gene_list_CanFam3.1.99gtf.txt",
-                         header = F)
+#retro_gene_list <- fread("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Retro_gene_finding/RetroGeneList/new_retro_gene_list_CanFam3.1.99gtf.txt",
+#                         header = F)
 
-whole_wes_table <- fread("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/arrange_table/whole_wes_table.txt") 
+whole_wes_table <- fread(#"G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/arrange_table/whole_wes_table.txt") 
+             "/Volumes/Research/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/arrange_table/whole_wes_table.txt")       
+
 exclude <- unique(unlist(whole_wes_table[The_reason_to_exclude!="Pass QC",.(Case_ID)]))
 
 
@@ -133,8 +135,7 @@ for (index in 1:length(tumor_type)) {
       if (other_variant != each_variant) {
         nubmersamples_others_variants <-
           length(unique(each_tumor_type[chrom_loc == other_variant, .(sample_names)][['sample_names']]))
-        numersamples_others_withoutvariants <-
-          length(unique(each_tumor_type$sample_names)) - others_variants
+        numersamples_others_withoutvariants <- length(unique(each_tumor_type$sample_names)) - nubmersamples_others_variants
         total_others <- total_others + nubmersamples_others_variants
         total_others_without <-
           total_others_without + numersamples_others_withoutvariants
@@ -149,7 +150,7 @@ for (index in 1:length(tumor_type)) {
       fisher.test(testor, alternative = "greater")$p.value
     
     info <- data.table(
-      tumor_type = each_tumor_type,
+      tumor_type = each_tumor,
       chrom_loc = each_variant,
       gene_name = gene_name,
       ensembl_id = ensembl_id,
@@ -171,7 +172,7 @@ fwrite(total_variant_summary,
        compress = "gzip",
        sep ="\t")
 
-
+a <- total_variant_summary[tumor_type=="MT"]
 
 ### Tumorwise genes
 sig_gene <- gene_total_info_sum[BH_pvalue<0.05,]
@@ -196,10 +197,8 @@ for (index in 1:length(tumor_type)) {
     
     for (other_ensembl in  total_ensembl_each_tumor) {
       if (other_ensembl != each_ensmbl) {
-        nubmersamples_others_variants <-
-          length(unique(each_tumor_type[ensembl_id == other_ensembl, .(sample_names)][['sample_names']]))
-        numersamples_others_withoutvariants <-
-          length(unique(each_tumor_type$sample_names)) - others_variants
+        nubmersamples_others_variants <- length(unique(each_tumor_type[ensembl_id == other_ensembl, .(sample_names)][['sample_names']]))
+        numersamples_others_withoutvariants <- length(unique(each_tumor_type$sample_names)) - nubmersamples_others_variants
         
         total_others <- total_others + nubmersamples_others_variants
         total_others_without <- total_others_without + numersamples_others_withoutvariants
@@ -214,7 +213,7 @@ for (index in 1:length(tumor_type)) {
       fisher.test(testor, alternative = "greater")$p.value
     
     info <- data.table(
-      tumor_type = each_tumor_type,
+      tumor_type = each_tumor,
       gene_name = gene_name,
       ensembl_id = each_ensmbl,
       p_value = each_ensembl_p_value
