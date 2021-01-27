f592b317951c9b80a0a978522aab4475ebc7e1a5
kunlin0814 <42413453+kunlin0814@users.noreply.github.com>
2021-01-27 01:15:19 -0500

    Update find_significat_gene.R

--- find_significant_gene_mutation/find_significat_gene.R
+++ find_significant_gene_mutation/find_significat_gene.R
@@ -1,17 +1,17 @@
 library(data.table)
 library(tidyverse)
 library(readxl)
-source(#"C:/Users/abc73/Documents/GitHub/R_util/my_util.R")
-  "/Volumes/Research/GitHub/R_util/my_util.R")
-base_dir <- "/Volumes/Research/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/VAF/Burair_filtering3/Mutect1"
-  #"G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/VAF/Burair_filtering3/Mutect1"
+source("C:/Users/abc73/Documents/GitHub/R_util/my_util.R")
+  #"/Volumes/Research/GitHub/R_util/my_util.R")
+base_dir <- #"/Volumes/Research/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/VAF/Burair_filtering3/Mutect1"
+  "G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/VAF/Burair_filtering3/Mutect1"
 seperator <- "/"
 
 #retro_gene_list <- fread("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Retro_gene_finding/RetroGeneList/new_retro_gene_list_CanFam3.1.99gtf.txt",
 #                         header = F)
 
-whole_wes_table <- fread(#"G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/arrange_table/whole_wes_table.txt") 
-             "/Volumes/Research/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/arrange_table/whole_wes_table.txt")       
+whole_wes_table <- fread("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/arrange_table/whole_wes_table.txt") 
+             #"/Volumes/Research/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/arrange_table/whole_wes_table.txt")       
 
 exclude <- unique(unlist(whole_wes_table[The_reason_to_exclude!="Pass QC",.(Case_ID)]))
 
@@ -105,20 +105,19 @@ for (index in 1:length(total_sample)) {
 
 ## TumorWise
 tumor_type <- unique(mutect_after_vaf$tumor_type)
-
 ### Tumorwise variants
 
 sig_variants <- total_info_sum[BH_pvalue < 0.05,]
 total_variant_summary <- NULL
 for (index in 1:length(tumor_type)) {
-  print(paste("processing the", index, "tumor, with total number tumor", length(tumor_type),sep = " "))
+  #print(paste("processing the", index, "tumor, with total number tumor", length(tumor_type),sep = " "))
   each_tumor <- tumor_type[index]
   each_tumor_info_sum <- NULL
   each_tumor_type <-
     sig_variants[tumor_type == each_tumor, .(chrom_loc, sample_names, gene_name, ensembl_id)]
   total_variants_each_tumor <- unique(each_tumor_type[["chrom_loc"]])
   for (i in 1:length(total_variants_each_tumor)) {
-    print(paste("processing the",i, "variants, with total variants", length(total_variants_each_tumor), sep = " "))
+    #print(paste("processing the",i, "variants, with total variants", length(total_variants_each_tumor), sep = " "))
     each_variant <- total_variants_each_tumor[i]
     total_others <- 0
     total_others_without <- 0
@@ -131,15 +130,15 @@ for (index in 1:length(tumor_type)) {
     ensembl_id <-
       each_tumor_type[chrom_loc == each_variant, .(ensembl_id)][['ensembl_id']][1]
     
-    for (other_variant in  total_variants_each_tumor) {
-      if (other_variant != each_variant) {
-        nubmersamples_others_variants <-
-          length(unique(each_tumor_type[chrom_loc == other_variant, .(sample_names)][['sample_names']]))
+    remaining_variant <- total_variants_each_tumor[!total_variants_each_tumor %in% each_variant]
+    
+    for (other_variant in  remaining_variant) {
+    
+        nubmersamples_others_variants <- length(unique(each_tumor_type[chrom_loc == other_variant, .(sample_names)][['sample_names']]))
         numersamples_others_withoutvariants <- length(unique(each_tumor_type$sample_names)) - nubmersamples_others_variants
         total_others <- total_others + nubmersamples_others_variants
-        total_others_without <-
-          total_others_without + numersamples_others_withoutvariants
-      }
+        total_others_without <- total_others_without + numersamples_others_withoutvariants
+      
       
     }
     testor <-
@@ -148,7 +147,9 @@ for (index in 1:length(tumor_type)) {
     
     each_variant_p_value <-
       fisher.test(testor, alternative = "greater")$p.value
-    
+    # if (each_variant_p_value <0.05){
+    # print(testor)
+    # print(each_variant_p_value)}
     info <- data.table(
       tumor_type = each_tumor,
       chrom_loc = each_variant,
@@ -227,13 +228,13 @@ for (index in 1:length(tumor_type)) {
 }
 
 
-fwrite(total_ensembl_summary,
-       file = paste(base_dir,"gene_tumorwise_p_value_Filtering3_VAF_Mutect_orientBias3.gz",sep = seperator)
-      ,col.names = T,row.names = F,
-       quote = F,
-      eol = "\n",
-      compress = "gzip",
-      sep ="\t")
+# fwrite(total_ensembl_summary,
+#        file = paste(base_dir,"gene_tumorwise_p_value_Filtering3_VAF_Mutect_orientBias3.gz",sep = seperator)
+#       ,col.names = T,row.names = F,
+#        quote = F,
+#       eol = "\n",
+#       compress = "gzip",
+#       sep ="\t")
 
 # 
 # ### check the results
