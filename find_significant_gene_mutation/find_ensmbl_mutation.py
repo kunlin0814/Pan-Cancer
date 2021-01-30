1760fc6769b663c9570fc88a76134969e1a1f855
kunlin0814 <42413453+kunlin0814@users.noreply.github.com>
2021-01-30 16:06:29 -0500

    update for sign gene callable

--- find_significant_gene_mutation/find_ensmbl_mutation.py
+++ find_significant_gene_mutation/find_ensmbl_mutation.py
@@ -8,54 +8,62 @@ Created on Mon Nov 25 15:52:10 2019
 """
 first argument is file path contains gene name and gene location
 second argument is the gene_name file path
+The output file will create a file that contains mutation number in a given gene list
+ex: id1 id2 id3
+    0   1   0
+    
 
 """
+
 import sys
 import pandas as pd
-
-candidate_info = r"C:\Users\abc73\Desktop\TestTMB\Pan_cancer_ensembl_location.txt"
-#sys.argv[1]
-#'C:\\Users\\abc73_000\\Desktop\\Retro_gene\\new_retro_gene_list_information_CanFam3.1.99gtf.txt' 
+candidate_info = sys.argv[1]
+# r"C:\Users\abc73\Desktop\TestTMB\Pan_cancer_sign_target_ensembl_location.txt"
 #
-mut_gene_info = r"C:\Users\abc73\Desktop\TestTMB\CMT-2.Mutect.vcf-5steps_orientBias3-avinput.exonic_variant_function_WithGeneName"
-#sys.argv[2]
-#'C:\\Users\\abc73_000\\Desktop\\Retro_gene\\DD0001_rg_added_sorted_dedupped_removed.MuTect.vcf-PASS_filteredMut' 
+#'C:\\Users\\abc73_000\\Desktop\\Retro_gene\\new_retro_gene_list_information_CanFam3.1.99gtf.txt'
 #
-sample_name ="CMT-2"
+mut_gene_info = sys.argv[2]
+#r"C:\Users\abc73\Desktop\TestTMB\CMT-2.Mutect.vcf-5steps_orientBias3-avinput.exonic_variant_function_WithGeneName"
+#
 
-output = open(r"C:\Users\abc73\Desktop\TestTMB\test_out.txt",'w')
+sample_name = sys.argv[3]
 
-ensembl_mut_sum ={}
-with open(candidate_info,'r') as f:
+output = open(sys.argv[4], 'w')
+
+ensembl_mut_sum = {}
+with open(candidate_info, 'r') as f:
     for each_line in f:
-        if each_line[0]!="#":
+        if each_line[0] != "#":
             ensembl = each_line.split("\t")[0]
             ensembl_mut_sum[ensembl] = 0
 
-mut_ensmbl_id =[]
+mut_ensmbl_id = []
 
-with open(mut_gene_info,'r') as f1:
+with open(mut_gene_info, 'r') as f1:
     for each_line in f1:
         ensembl = each_line.split("\t")[3].split(":")[0]
         mut_ensmbl_id.append(ensembl)
 
-     
+
 for i in mut_ensmbl_id:
     if i in ensembl_mut_sum.keys():
-        ensembl_mut_sum[i] +=1
+        ensembl_mut_sum[i] += 1
 
 output_gene = sorted(list(ensembl_mut_sum.keys()))
-output.write("sample_names"+"\t")
 
-
-output.write("\t".join(output_gene))
-output.write("\n")
+# output.write("sample_names"+"\t")
+# output.write("\t".join(output_gene))
+# output.write("\n")
 output.write(sample_name+"\t")
+
 
 output_value = []
 for i in output_gene:
     value = str(ensembl_mut_sum[i])
     output_value.append(value)
 output.write("\t".join(output_value))
-output.write("\n")   
+output.write("\n")
 
+
+# check = pd.read_csv(r"C:\Users\abc73\Desktop\TestTMB\new_test_out.txt",sep ="\t",
+#                     header =None)
