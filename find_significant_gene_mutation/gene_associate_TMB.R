0aad9f0b6387b4e338293e4b2f7b7225d3807f6c
kunlin0814 <42413453+kunlin0814@users.noreply.github.com>
2021-03-05 21:01:07 -0500

    add p53 cell cycle gene

--- find_significant_gene_mutation/gene_associate_TMB.R
+++ find_significant_gene_mutation/gene_associate_TMB.R
@@ -330,185 +330,7 @@ total_tumor_gene_sum <- na.omit(total_tumor_gene_sum)
 fwrite(total_tumor_gene_sum, file = paste(output_dir,"03_02","tmb_h_Not_include_amp_candidate_gene_associated_TMB_03_02_summary.txt",sep = seperator),
        col.names = T, row.names = F, quote = F, sep = "\t", eol = "\n",na = "NA")
 
-
-############################ TMB-l and TMB-h gene associated end ############################
-
-### now seperate snv and amp
-base_dir <- 
-  #"/Volumes/Research/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/VAF/New_Burair_filterin3/Mutect1"
-  "G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/Oncoprint_analysis"
-seperator <- "/"
-
-whole_wes_clean_breed_table <- fread("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/arrange_table/whole_wes_table_02_19.txt") 
-exclude <- unique(unlist(whole_wes_clean_breed_table[The_reason_to_exclude!="Pass QC",.(Case_ID)]))
-
-amp_delete <- fread(paste(base_dir,"CNV_Taifang_total_amp_delete_no_pseudo_subtype_02_18.txt",sep = seperator),
-                    header = T,na.strings = "")
-
-amp_delete <- amp_delete[!sample_names %in% exclude]
-amp_delete <- amp_delete[!grepl("ENSCAFG",amp_delete[,.(gene_name)]$gene_name,ignore.case = T)]
-CNV <- unique(amp_delete[,c("sample_names","gene_name","mut_type","subtype"),with =F])
-colnames(CNV)<- c("sample_names","gene_name","status","Subtype")
-CNV <- CNV[!sample_names %in% exclude & Subtype!="UCL",]
-#amp_delete_data <- amp_delete[gene_name %in% target_pathway_gene$target_pathway_gene,.(sample_names,gene_name,mut_type,subtype)]
-total_cnv <- rbindlist(list(CNV))
-
-all_tumor_cut <- 10
-signle_tumor_cut <- 5
-
-all_tumor_type <- unique(total_cnv$Subtype)
-# might only have one sample has that
-total_tumor_gene_sum <- NULL
-for( index in 1:length(all_tumor_type)){
-  print(paste("processing the",index,"tumor, with total tumors",length(all_tumor_type),sep = " " ))
-  each_tumor <- all_tumor_type[index]
-  each_tumor_gene_info <- unique(total_cnv[Subtype == each_tumor, .(gene_name)][["gene_name"]])
-  each_tumor_gene_candidate <- NULL
-  target_gene_sample_number <- NULL
-  target_gene_total_sample_number <- NULL
-  for (gene_index in 1:length(each_tumor_gene_info)){
-    each_gene <- each_tumor_gene_info[gene_index]
-    # each_gene <- "PIK3CA"
-    total_sample <- unique(total_cnv[gene_name==each_gene, .(sample_names,Subtype)])
-    total_sample_sum <- as.data.table(table(total_sample$Subtype))
-    total_sample_number <- sum(total_sample_sum$N)
-    each_tumor_sample_sum <- total_sample_sum[which(total_sample_sum$V1==each_tumor)]$N
-    if (each_tumor_sample_sum >= signle_tumor_cut && total_sample_number>= all_tumor_cut){
-      target_gene_sample_number <- c(target_gene_sample_number,each_tumor_sample_sum)
-      target_gene_total_sample_number <- c(target_gene_total_sample_number,total_sample_number)
-      each_tumor_gene_candidate <- c(each_tumor_gene_candidate,each_gene)
-    }
-  }
-  each_tumor_sum <-  data.table(Subtype = each_tumor,
-                                gene_name = each_tumor_gene_candidate,
-                                target_gene_sample_number = target_gene_sample_number,
-                                target_gene_total_sample_number = target_gene_total_sample_number) 
-  
-  total_tumor_gene_sum <- rbindlist(list(total_tumor_gene_sum,each_tumor_sum),fill = T)
-}
-total_tumor_gene_sum <- na.omit(total_tumor_gene_sum)
-fwrite(total_tumor_gene_sum, file = paste(output_dir,"03_02","amp_candidate_gene_associated_TMB_03_02.txt",sep = seperator),
-       col.names = T, row.names = F, quote = F, sep = "\t", eol = "\n",na = "NA")
-
-## Use candidate gene to compare tmb
-
-## exclude s1 high
-# s1_data <- fread("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/arrange_table/S1_high_low.txt")
-# s1_high_sample <- s1_data[S1_Status=="S1 high"]$SampleName
-# exclude <- c(exclude, s1_high_sample)
-
-## excldue NA and UCL
-tmb_l <- c("MT", "GLM","BCL")
-tmb_h <- c("OM","OSA","HSA","TCL")
-
-# total_tumor_gene_sum <- fread(paste(output_dir,"03_02","Not_include_amp_candidate_gene_associated_TMB_03_02.txt",sep = seperator))
-# 
-total_tumor_gene_sum <- na.omit(total_tumor_gene_sum)
-
-## append TMB info
-TMB_info <- fread("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/Mut_rate/With_Breeds_exclude_failQC_TMB_Burair_filtering3_02_11.txt")
-colnames(TMB_info)
-total_cnv$tmb <- match_vector_table(total_cnv$sample_names, "combine_snv_indel_tmb", TMB_info, string_value = F)
-total_tumor_type <- unique(total_cnv$Subtype)
-
-## Normalize TMB with regards to each tumor median
-## now decide not normalize 2/25
-# total_tumor_normalize <- NULL
-# 
-# for (each_tumor in total_tumor_type){
-#   each_tumor_info <- total_cnv[Subtype==each_tumor,]
-#   each_median <- median(total_cnv[Subtype==each_tumor, .(tmb)][['tmb']])
-#   # each_sd <- sd(total_cnv[Subtype==each_tumor, .(tmb)][['tmb']]) 
-#   each_tumor_info <- each_tumor_info[, normalizetmb:= (tmb/each_median)+0.1]
-#   #each_tumor_info <- each_tumor_info[, normalizetmb:= tmb]
-#   total_tumor_normalize <- rbindlist(list(total_tumor_normalize,each_tumor_info))
-# }
-# 
-# total_cnv <- total_tumor_normalize
-
-## Normalize end 
-
-candidate_gene <- unique(total_tumor_gene_sum$gene_name)
-# each tumor type seperate tmbl and tmbh
-total_gene_sum <- NULL
-for (each_gene in candidate_gene){
-  #print(each_gene)
-  tmb_l_group <- total_cnv[Subtype %in%tmb_l, ]
-  tmb_l_group_total_samples <- unique(tmb_l_group$sample_names)
-  tmb_l_gene_mut_samples <- unique(tmb_l_group[gene_name==each_gene,.(sample_names)][["sample_names"]])
-  tmb_l_gene_no_mut_samples <- unique(setdiff(tmb_l_group_total_samples,tmb_l_gene_mut_samples))
-  tmb_l_gene_mut_tmb <- unique(total_cnv[sample_names %in% tmb_l_gene_mut_samples,.(sample_names,tmb)])[["tmb"]]
-  tmb_l_gene_no_mut_tmb <- unique(total_cnv[sample_names %in% tmb_l_gene_no_mut_samples,.(sample_names,tmb)])[["tmb"]]
-  
-  
-  if (length(tmb_l_gene_mut_samples)!=0 && length(tmb_l_gene_no_mut_samples)!=0){
-    tmb_l_test <- wilcox.test(tmb_l_gene_mut_tmb,tmb_l_gene_no_mut_tmb)
-    tmb_l_pvalue <- tmb_l_test$p.value
-    median_tmb_l_gene_mut_tmb <- median(tmb_l_gene_mut_tmb)
-    median_tmb_l_gene_no_mut_tmb <- median(tmb_l_gene_no_mut_tmb)
-    tmb_l_fold_change = median_tmb_l_gene_mut_tmb/median_tmb_l_gene_no_mut_tmb
-  }
-  # else{
-  #   tmb_l_pvalue <- "No tmb_l_gene_mut_samples or tmb_l_gene_no_mut_samples"
-  #   median_tmb_l_gene_mut_tmb <- "No tmb_l_gene_mut_samples or tmb_l_gene_no_mut_samples"
-  #   median_tmb_l_gene_no_mut_tmb <-"No tmb_l_gene_mut_samples or tmb_l_gene_no_mut_samples"
-  # }
-  
-  tmb_h_group <- total_cnv[Subtype %in%tmb_h, ]
-  tmb_h_group_total_samples <- unique(tmb_h_group$sample_names)
-  tmb_h_gene_mut_samples <- unique(tmb_h_group[gene_name==each_gene,.(sample_names)][["sample_names"]])
-  tmb_h_gene_no_mut_samples <- setdiff(tmb_h_group_total_samples,tmb_h_gene_mut_samples)
-  tmb_h_gene_mut_tmb <- unique(total_cnv[sample_names %in% tmb_h_gene_mut_samples,.(sample_names,tmb)])[["tmb"]]
-  tmb_h_gene_no_mut_tmb <- unique(total_cnv[sample_names %in% tmb_h_gene_no_mut_samples,.(sample_names,tmb)])[["tmb"]]
-  
-  if (length(tmb_h_gene_mut_samples)!=0 && length(tmb_h_gene_no_mut_samples)!=0){
-    
-    tmb_h_test <- wilcox.test(tmb_h_gene_mut_tmb,tmb_h_gene_no_mut_tmb)
-    tmb_h_pvalue <- tmb_h_test$p.value
-    median_tmb_h_gene_mut_tmb <- median(tmb_h_gene_mut_tmb)
-    median_tmb_h_gene_no_mut_tmb <- median(tmb_h_gene_no_mut_tmb)
-    tmb_h_fold_change = median_tmb_h_gene_mut_tmb/median_tmb_h_gene_no_mut_tmb
-  }
-  
-  # else{
-  #   tmb_h_pvalue <-"No tmb_h_gene_mut_samples or tmb_h_gene_no_mut_samples"
-  #   median_tmb_h_gene_mut_tmb <- "No tmb_h_gene_mut_samples or tmb_h_gene_no_mut_samples"
-  #   median_tmb_h_gene_no_mut_tmb <-"No tmb_h_gene_mut_samples or tmb_h_gene_no_mut_samples"
-  # }
-  
-  # if (length(tmb_h_gene_mut_samples)!=0 && length(tmb_h_gene_no_mut_samples)!=0 
-  #     && length(tmb_l_gene_mut_samples)!=0 && length(tmb_l_gene_no_mut_samples)!=0){
-  each_gene_sum <- list(gene = each_gene,
-                        median_tmb_l_gene_mut_tmb=median_tmb_l_gene_mut_tmb,
-                        median_tmb_l_gene_no_mut_tmb=median_tmb_l_gene_no_mut_tmb,
-                        median_tmb_h_gene_mut_tmb= median_tmb_h_gene_mut_tmb,
-                        median_tmb_h_gene_no_mut_tmb=median_tmb_h_gene_no_mut_tmb,
-                        tmb_l_number_samples=length(tmb_l_gene_mut_samples),
-                        tmb_l_gene_no_mut_samples = length(tmb_l_gene_no_mut_samples),
-                        tmb_h_gene_number_samples = length(tmb_h_gene_mut_samples),
-                        tmb_h_gene_no_mut_samples = length(tmb_h_gene_no_mut_samples),
-                        tmb_l_fold_change = tmb_l_fold_change, 
-                        tmb_h_fold_change = tmb_h_fold_change,
-                        tmb_l_pvalue = tmb_l_pvalue,
-                        tmb_h_pvalue = tmb_h_pvalue)
-  
-  
-  total_gene_sum <- rbindlist(list(total_gene_sum,each_gene_sum))
-}
-total_gene_sum <- setDT(total_gene_sum)
-total_gene_sum <- total_gene_sum[order(tmb_l_pvalue)]
-total_gene_sum$tmb_l_BH_pvalue <-  p.adjust(total_gene_sum$tmb_l_pvalue, method = "BH")
-total_gene_sum <- total_gene_sum[order(tmb_h_pvalue)]
-total_gene_sum$tmb_h_BH_pvalue <- p.adjust(total_gene_sum$tmb_h_pvalue,method = "BH")
-
-
-fwrite(total_gene_sum, file = paste(output_dir,"03_02","amp_not_normalize_p_value_candidate_gene_associated_TMB_03_02.txt",sep = seperator),
-       col.names = T, row.names = F, quote = F, sep = "\t", eol = "\n",na = "NA")
-
-
-
-#################### Combine SNV CNV indel  ################################
-## 03_02 only examine pathway tp53 and cell cycle
+### P53 and cell cycle gene 
 
 base_dir <- 
   #"/Volumes/Research/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/VAF/New_Burair_filterin3/Mutect1"
@@ -518,6 +340,7 @@ seperator <- "/"
 output_dir <- "G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/Mut_rate/Gene_association_TMB/"
 
 pathway <- fread(paste(base_dir,"all_pathway.txt",sep = seperator), na.strings = "")
+
 target_path_way <- pathway[,c("TP53","Cell cycle"), with =F]
 target_pathway_gene <- NULL
 for (each_pathway in colnames(target_path_way)){
@@ -525,9 +348,6 @@ for (each_pathway in colnames(target_path_way)){
   each_path_clean_gene <- each_path_gene[!is.na(each_path_gene)]
   target_pathway_gene <- c(target_pathway_gene,each_path_clean_gene)
 }
-
-
-#target_pathway_gene <- fread(paste(base_dir,"target_pathway_total_genes.txt",sep = seperator), na.strings = "")
 
 whole_wes_clean_breed_table <- fread("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/arrange_table/whole_wes_table_02_19.txt") 
 exclude <- unique(unlist(whole_wes_clean_breed_table[The_reason_to_exclude!="Pass QC",.(Case_ID)]))
@@ -556,15 +376,40 @@ CNV <- unique(amp_delete[,c("sample_names","gene_name","mut_type","subtype"),wit
 colnames(CNV)<- c("sample_names","gene_name","status","Subtype")
 CNV <- CNV[gene_name %in% target_pathway_gene, ]
 
-## exclude s1 high
+## exclude s1 high and UCL
 s1_data <- fread("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/arrange_table/S1_high_low.txt")
 s1_high_sample <- s1_data[S1_Status=="S1 high"]$SampleName
 exclude <- c(exclude, s1_high_sample)
 ##
 total_snv_cnv <- rbindlist(list(SNV,indel_file, CNV))
-total_snv_cnv <- total_snv_cnv[!sample_names %in% exclude & Subtype !="UCL",]
+total_snv_cnv <- total_snv_cnv[!sample_names %in% exclude & Subtype !="UCL" & gene_name %in% target_pathway_gene,]
+
+## append tmb info
+
+TMB_info <- fread(#"/Volumes/Research/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/Mut_rate/With_Breeds_exclude_failQC_TMB_Burair_filtering3_02_11.txt")
+  "G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/Mut_rate/With_Breeds_exclude_failQC_TMB_Burair_filtering3_02_11.txt")
+total_snv_cnv$tmb <- match_vector_table(total_snv_cnv$sample_names, "combine_snv_indel_tmb", TMB_info, string_value = F)
+
+## Normalize TMB with regards to each tumor median (in breed associated analysis)
+all_tumor_type <- unique(total_snv_cnv$Subtype)
+total_tumor_normalize <- NULL
+
+for (each_tumor in all_tumor_type){
+  each_tumor_info <- total_snv_cnv[Subtype==each_tumor,]
+  each_median <- median(total_snv_cnv[Subtype==each_tumor, .(tmb)][['tmb']])
+  # each_sd <- sd(total_snv_cnv[Subtype==each_tumor, .(tmb)][['tmb']])
+  each_tumor_info <- each_tumor_info[, normalizetmb:= (tmb/each_median)+0.1]
+  #each_tumor_info <- each_tumor_info[, normalizetmb:= tmb]
+  total_tumor_normalize <- rbindlist(list(total_tumor_normalize,each_tumor_info))
+}
+
+total_snv_cnv <- total_tumor_normalize
+
 
 all_tumor_cut <- 0
+signle_tumor_cut <- 5
+
+all_tumor_cut <- 10
 signle_tumor_cut <- 5
 
 all_tumor_type <- unique(total_snv_cnv$Subtype)
@@ -582,14 +427,12 @@ for( index in 1:length(all_tumor_type)){
     # each_gene <- "PIK3CA"
     total_sample <- unique(total_snv_cnv[gene_name==each_gene, .(sample_names,Subtype)])
     total_sample_sum <- as.data.table(table(total_sample$Subtype))
-    #total_sample_number <- sum(total_sample_sum$N)
+    total_sample_number <- sum(total_sample_sum$N)
     each_tumor_sample_sum <- total_sample_sum[which(total_sample_sum$V1==each_tumor)]$N
-    
     if (each_tumor_sample_sum >= signle_tumor_cut && total_sample_number>= all_tumor_cut){
-      candidate_gene <- each_gene
       target_gene_sample_number <- c(target_gene_sample_number,each_tumor_sample_sum)
-     
-      each_tumor_gene_candidate <- c(each_tumor_gene_candidate,candidate_gene)
+      target_gene_total_sample_number <- c(target_gene_total_sample_number,total_sample_number)
+      each_tumor_gene_candidate <- c(each_tumor_gene_candidate,each_gene)
     }
   }
   each_tumor_sum <-  data.table(Subtype = each_tumor,
@@ -600,7 +443,7 @@ for( index in 1:length(all_tumor_type)){
   total_tumor_gene_sum <- rbindlist(list(total_tumor_gene_sum,each_tumor_sum),fill = T)
 }
 total_tumor_gene_sum <- na.omit(total_tumor_gene_sum)
-fwrite(total_tumor_gene_sum, file = paste(output_dir,"03_02","include_amp_candidate_gene_associated_TMB_03_02.txt",sep = seperator),
+fwrite(total_tumor_gene_sum, file = paste(output_dir,"02_25","include_amp_candidate_gene_associated_TMB_02_25.txt",sep = seperator),
        col.names = T, row.names = F, quote = F, sep = "\t", eol = "\n",na = "NA")
 
 ## Use candidate gene to compare tmb
@@ -614,15 +457,15 @@ fwrite(total_tumor_gene_sum, file = paste(output_dir,"03_02","include_amp_candid
 tmb_l <- c("MT", "GLM","BCL")
 tmb_h <- c("OM","OSA","HSA","TCL")
 
-# total_tumor_gene_sum <- fread(paste(output_dir,"03_02","Not_include_amp_candidate_gene_associated_TMB_03_02.txt",sep = seperator))
+# total_tumor_gene_sum <- fread(paste(output_dir,"02_25","Not_include_amp_candidate_gene_associated_TMB_02_25.txt",sep = seperator))
 # 
 total_tumor_gene_sum <- na.omit(total_tumor_gene_sum)
 
 ## append TMB info
 TMB_info <- fread("G:/MAC_Research_Data/Pan_cancer/Pan_cancer-analysis/Mutation_rate_VAF/Mut_rate/With_Breeds_exclude_failQC_TMB_Burair_filtering3_02_11.txt")
 colnames(TMB_info)
-total_snv_cnv$tmb <- match_vector_table(total_snv_cnv$sample_names, "combine_snv_indel_tmb", TMB_info, string_value = F)
-total_tumor_type <- unique(total_snv_cnv$Subtype)
+total_cnv$tmb <- match_vector_table(total_cnv$sample_names, "combine_snv_indel_tmb", TMB_info, string_value = F)
+total_tumor_type <- unique(total_cnv$Subtype)
 
 candidate_gene <- unique(total_tumor_gene_sum$gene_name)
 # each tumor type seperate tmbl and tmbh
@@ -697,12 +540,8 @@ total_gene_sum$tmb_l_BH_pvalue <-  p.adjust(total_gene_sum$tmb_l_pvalue, method 
 total_gene_sum <- total_gene_sum[order(tmb_h_pvalue)]
 total_gene_sum$tmb_h_BH_pvalue <- p.adjust(total_gene_sum$tmb_h_pvalue,method = "BH")
 
-a = total_gene_sum[total_gene_sum$gene=="MDM2",]
 
-
-fwrite(total_gene_sum, file = paste(output_dir,"03_02","include_amp_not_normalize_p_value_candidate_gene_associated_TMB_03_02.txt",sep = seperator),
+fwrite(total_gene_sum, file = paste(output_dir,"02_25","include_amp_not_normalize_p_value_candidate_gene_associated_TMB_02_25.txt",sep = seperator),
        col.names = T, row.names = F, quote = F, sep = "\t", eol = "\n",na = "NA")
-
-
 
 
