0bac0d3ee158a41cf0fe01342599be6a952dd44d
kunlin0814 <42413453+kunlin0814@users.noreply.github.com>
2021-03-24 23:27:20 -0400

    Update analyzedGtf.R

--- analyzedGtf.R
+++ analyzedGtf.R
@@ -5,12 +5,31 @@ library(ggrepel)
 library(readxl)
 library(ggpubr)
 library(grid)
+source("C:/Users/abc73/Documents/GitHub/R_util/my_util.R")
+# gtf <- fread("G:\\MAC_Research_Data\\Pan_cancer\\Canine_Mapping_source\\Canis_familiaris.CanFam3.1.99.chr.gtf",
+#              skip =5)
+human_gtf <- fread("G:/MAC_Research_Data/Pan_cancer/Homo_sapiens.GRCh38.103.gtf/Homo_sapiens.GRCh38.103.gtf",
+                   skip = 5)
 
-gtf <- fread("G:\\MAC_Research_Data\\Pan_cancer\\Canine_Mapping_source\\Canis_familiaris.CanFam3.1.99.chr.gtf",
-             skip =5)
+
+human <- human_gtf[V3 =="gene",.(V1,V3,V4,V5,V9)]
+
+sep_gene_stp1 <- as.data.table(str_split_fixed(human$V9," ",2))
+
+sep_gene_stp2 <- as.data.table(str_split_fixed(sep_gene_stp1$V2,";",5))
+
+gene_name_table <- as.data.table(str_split_fixed(sep_gene_stp2$V3,"gene_name",2))
+pure_ensembl <- gsub('[\"]','',sep_gene_stp2$V1)
+pure_gene <- gsub('[\ "]','',gene_name_table$V2)
+
+human_gene_ensembl <- data.table(ensembl = pure_ensembl,
+                                    gene_name = pure_gene)
+fwrite(human_gene_ensembl,
+       file ='G:/MAC_Research_Data/Pan_cancer/Pan_Cancer_paper/Human/OM/human_ensembl_gene.txt',
+       col.names = T,row.names = F, quote = F, sep = "\t",
+       eol = "\n")
 
 
-a <- gtf[V3 =="exon",.(V1,V3,V4,V5)]
 a <- unique(a)
 a$diff <- a$V5-a$V4
 sum(a$diff)
