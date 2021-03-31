8ae8ae5d2f05c554760f9fd4f0695ce39e2daf69
kunlin0814 <42413453+kunlin0814@users.noreply.github.com>
2021-03-31 17:22:05 -0400

    Update Bam_start_merge_DD0001_Picard_Mutect_Annovar.sh

--- WES/Bam_start_merge_DD0001_Picard_Mutect_Annovar.sh
+++ WES/Bam_start_merge_DD0001_Picard_Mutect_Annovar.sh
@@ -43,69 +43,69 @@ mkdir -p $strelka_out
 # samtools merge ${results}/${Tumor_Run}.bam /scratch/kh31516/Melanoma_new/WES/results/DD0001-1/ERR1681500.bam /scratch/kh31516/Melanoma_new/WES/results/DD0001-2/ERR1681455.bam  
 
 
-####### Picard #######
-# get header information
-samtools view -H ${results}/${Normal_Run}.bam > $results/header_N
-samtools view -H ${results}/${Tumor_Run}.bam > $results/header_T
+# ####### Picard #######
+# # get header information
+# samtools view -H ${results}/${Normal_Run}.bam > $results/header_N
+# samtools view -H ${results}/${Tumor_Run}.bam > $results/header_T
 
-# exclude unmapped reads based on FLAG
-samtools view $results/${Normal_Run}.bam | awk 'BEGIN{FS="\t";OFS="\t"}{if ($2%16==3){print $0}}' > $results/${Normal_Run}-cleaned.sam
-cat $results/header_N $results/${Normal_Run}-cleaned.sam > $results/fooN
-mv $results/fooN $results/${Normal_Run}-cleaned.sam
-samtools view $results/${Tumor_Run}.bam | awk 'BEGIN{FS="\t";OFS="\t"}{if ($2%16==3){print $0}}' > $results/${Tumor_Run}-cleaned.sam
-cat $results/header_T $results/${Tumor_Run}-cleaned.sam > $results/fooT
-mv $results/fooT $results/${Tumor_Run}-cleaned.sam
+# # exclude unmapped reads based on FLAG
+# samtools view $results/${Normal_Run}.bam | awk 'BEGIN{FS="\t";OFS="\t"}{if ($2%16==3){print $0}}' > $results/${Normal_Run}-cleaned.sam
+# cat $results/header_N $results/${Normal_Run}-cleaned.sam > $results/fooN
+# mv $results/fooN $results/${Normal_Run}-cleaned.sam
+# samtools view $results/${Tumor_Run}.bam | awk 'BEGIN{FS="\t";OFS="\t"}{if ($2%16==3){print $0}}' > $results/${Tumor_Run}-cleaned.sam
+# cat $results/header_T $results/${Tumor_Run}-cleaned.sam > $results/fooT
+# mv $results/fooT $results/${Tumor_Run}-cleaned.sam
 
-####### Picard #######
-# picard sort
-cd ${results}
-module load picard/2.21.6-Java-11
+# ####### Picard #######
+# # picard sort
+# cd ${results}
+# module load picard/2.21.6-Java-11
 
-java -jar $EBROOTPICARD/picard.jar AddOrReplaceReadGroups I=${results}/${Normal_Run}-cleaned.sam \
-O=${results}/${Normal_Run}_rg_added_sorted.bam SO=coordinate RGID=4 RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM=${Normal_Run}_normal
+# java -jar $EBROOTPICARD/picard.jar AddOrReplaceReadGroups I=${results}/${Normal_Run}-cleaned.sam \
+# O=${results}/${Normal_Run}_rg_added_sorted.bam SO=coordinate RGID=4 RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM=${Normal_Run}_normal
 
-java -jar $EBROOTPICARD/picard.jar AddOrReplaceReadGroups I=${results}/${Tumor_Run}-cleaned.sam \
-O=${results}/${Tumor_Run}_rg_added_sorted.bam SO=coordinate RGID=4 RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM=${Tumor_Run}_tumor
+# java -jar $EBROOTPICARD/picard.jar AddOrReplaceReadGroups I=${results}/${Tumor_Run}-cleaned.sam \
+# O=${results}/${Tumor_Run}_rg_added_sorted.bam SO=coordinate RGID=4 RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM=${Tumor_Run}_tumor
 
-# picard MarkDuplicates
-java -jar $EBROOTPICARD/picard.jar  MarkDuplicates I=${results}/${Normal_Run}_rg_added_sorted.bam \
-O=${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.bam CREATE_INDEX=true \
-VALIDATION_STRINGENCY=SILENT M=${results}/${Normal_Run}-output.metrics REMOVE_SEQUENCING_DUPLICATES=true
+# # picard MarkDuplicates
+# java -jar $EBROOTPICARD/picard.jar  MarkDuplicates I=${results}/${Normal_Run}_rg_added_sorted.bam \
+# O=${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.bam CREATE_INDEX=true \
+# VALIDATION_STRINGENCY=SILENT M=${results}/${Normal_Run}-output.metrics REMOVE_SEQUENCING_DUPLICATES=true
 
-java -jar $EBROOTPICARD/picard.jar  MarkDuplicates I=${results}/${Tumor_Run}_rg_added_sorted.bam \
-O=${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.bam CREATE_INDEX=true \
-VALIDATION_STRINGENCY=SILENT M=${results}/${Tumor_Run}-output.metrics REMOVE_SEQUENCING_DUPLICATES=true
+# java -jar $EBROOTPICARD/picard.jar  MarkDuplicates I=${results}/${Tumor_Run}_rg_added_sorted.bam \
+# O=${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.bam CREATE_INDEX=true \
+# VALIDATION_STRINGENCY=SILENT M=${results}/${Tumor_Run}-output.metrics REMOVE_SEQUENCING_DUPLICATES=true
 
-# ####### Remove unneeded files #######
-# rm $results/${Normal_Run}.sam $results/ERR1681500.sam 
-# rm $results/header_N $results/header_T
-# rm $results/${Normal_Run}_rg_added_sorted.bam $results/${Tumor_Run}_rg_added_sorted.bam
-# rm $results/${Normal_Run}-cleaned.sam $results/${Tumor_Run}-cleaned.sam
+# # ####### Remove unneeded files #######
+# # rm $results/${Normal_Run}.sam $results/ERR1681500.sam 
+# # rm $results/header_N $results/header_T
+# # rm $results/${Normal_Run}_rg_added_sorted.bam $results/${Tumor_Run}_rg_added_sorted.bam
+# # rm $results/${Normal_Run}-cleaned.sam $results/${Tumor_Run}-cleaned.sam
 
-ml GATK/3.8-1-Java-1.8.0_144
-####### GATK Realign #######
-# Generating interval file for sort.bam
+# ml GATK/3.8-1-Java-1.8.0_144
+# ####### GATK Realign #######
+# # Generating interval file for sort.bam
 
-java -jar $EBROOTGATK/GenomeAnalysisTK.jar -T RealignerTargetCreator -R ${reference}/canFam3.fa \
--I ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.bam \
--nt $thread \
--o ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.bam.intervals --allow_potentially_misencoded_quality_scores
+# java -jar $EBROOTGATK/GenomeAnalysisTK.jar -T RealignerTargetCreator -R ${reference}/canFam3.fa \
+# -I ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.bam \
+# -nt $thread \
+# -o ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.bam.intervals --allow_potentially_misencoded_quality_scores
 
-java -jar $EBROOTGATK/GenomeAnalysisTK.jar -T RealignerTargetCreator -R ${reference}/canFam3.fa \
--I ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.bam \
--nt $thread \
--o ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.bam.intervals --allow_potentially_misencoded_quality_scores
+# java -jar $EBROOTGATK/GenomeAnalysisTK.jar -T RealignerTargetCreator -R ${reference}/canFam3.fa \
+# -I ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.bam \
+# -nt $thread \
+# -o ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.bam.intervals --allow_potentially_misencoded_quality_scores
 
-#### IndelRealigner realign ######
-java -jar $EBROOTGATK/GenomeAnalysisTK.jar -T IndelRealigner -R ${reference}/canFam3.fa \
--I ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.bam \
--targetIntervals ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.bam.intervals \
--o ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.realigned.bam --allow_potentially_misencoded_quality_scores
+# #### IndelRealigner realign ######
+# java -jar $EBROOTGATK/GenomeAnalysisTK.jar -T IndelRealigner -R ${reference}/canFam3.fa \
+# -I ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.bam \
+# -targetIntervals ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.bam.intervals \
+# -o ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.realigned.bam --allow_potentially_misencoded_quality_scores
 
-java -jar $EBROOTGATK/GenomeAnalysisTK.jar -T IndelRealigner -R ${reference}/canFam3.fa \
--I ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.bam \
--targetIntervals ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.bam.intervals \
--o ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.realigned.bam --allow_potentially_misencoded_quality_scores
+# java -jar $EBROOTGATK/GenomeAnalysisTK.jar -T IndelRealigner -R ${reference}/canFam3.fa \
+# -I ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.bam \
+# -targetIntervals ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.bam.intervals \
+# -o ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.realigned.bam --allow_potentially_misencoded_quality_scores
 
 # ######## Mutect #######
 # # Current Mutect version uses Java 1.7, while GATK requires Java 1.8. 
@@ -180,76 +180,76 @@ java -jar $EBROOTGATK/GenomeAnalysisTK.jar -T IndelRealigner -R ${reference}/can
 
 
 
-####### Mutect2 #######
+# ####### Mutect2 #######
 
-cd ${MuTect2_out}
+# cd ${MuTect2_out}
 
-module load SAMtools/1.9-GCC-8.3.0
-module load GATK/4.1.6.0-GCCcore-8.3.0-Java-1.8
+# module load SAMtools/1.9-GCC-8.3.0
+# module load GATK/4.1.6.0-GCCcore-8.3.0-Java-1.8
 
-Normal_sample=$(samtools view -H ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.realigned.bam | grep '@RG' |awk -F "SM:" '{print $2}' | cut -f1)
-Tumor_sample=$(samtools view -H ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.realigned.bam | grep '@RG' |awk -F "SM:" '{print $2}' | cut -f1)
+# Normal_sample=$(samtools view -H ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.realigned.bam | grep '@RG' |awk -F "SM:" '{print $2}' | cut -f1)
+# Tumor_sample=$(samtools view -H ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.realigned.bam | grep '@RG' |awk -F "SM:" '{print $2}' | cut -f1)
 
-## Mutect2 follows the Glioma PanCancer parameters
-gatk Mutect2 \
--R ${reference}/canFam3.fa \
--I ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.realigned.bam \
--I ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.realigned.bam \
--normal $Normal_sample \
---callable-depth 8 \
---panel-of-normals  $MuTect2_source/pon.vcf.gz \
---initial-tumor-lod 2.0 --normal-lod 2.2 --tumor-lod-to-emit 3.0 --pcr-indel-model CONSERVATIVE \
---f1r2-tar-gz ${MuTect2_out}/${SampleName}-f1r2.tar.gz \
---dont-use-soft-clipped-bases true \
--L ${MuTect2_source}/Uniq-Canis_familiaris.CanFam3.1.99.gtf-chr1-38X-CDS-forDepthOfCoverage.interval.list \
--O ${MuTect2_out}/${SampleName}_MuTect2_GATK4_noDBSNP.vcf
-
-
-## pass this raw data to LearnReadOrientationModel:
-gatk LearnReadOrientationModel -I ${MuTect2_out}/${SampleName}-f1r2.tar.gz -O ${MuTect2_out}/read-orientation-model.tar.gz
-
-# filtered Mutect2 files
-
-gatk FilterMutectCalls -R ${reference}/canFam3.fa \
-o=-priors ${MuTect2_out}/read-orientation-model.tar.gz \
--V ${MuTect2_out}/${SampleName}_MuTect2_GATK4_noDBSNP.vcf \
-O=${MuTect2_out}/filtered-${SampleName}_MuTect2_GATK4_noDBSNP.vcf
-
-cd ${MuTect2_out}
-
-### filter Mutect2 #####
-
-## with DbSNP
-java -Xmx32g -cp  $script/ DbSNP_filtering \
-${reference}/DbSNP_canFam3_version151-DogSD_Feb2020_V4.vcf \
-${MuTect2_out}/filtered-${SampleName}_MuTect2_GATK4_noDBSNP.vcf \
-${MuTect2_out}/DbSNP_filtered-${SampleName}_MuTect2_GATK4.vcf
+# ## Mutect2 follows the Glioma PanCancer parameters
+# gatk Mutect2 \
+# -R ${reference}/canFam3.fa \
+# -I ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.realigned.bam \
+# -I ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.realigned.bam \
+# -normal $Normal_sample \
+# --callable-depth 8 \
+# --panel-of-normals  $MuTect2_source/pon.vcf.gz \
+# --initial-tumor-lod 2.0 --normal-lod 2.2 --tumor-lod-to-emit 3.0 --pcr-indel-model CONSERVATIVE \
+# --f1r2-tar-gz ${MuTect2_out}/${SampleName}-f1r2.tar.gz \
+# --dont-use-soft-clipped-bases true \
+# -L ${MuTect2_source}/Uniq-Canis_familiaris.CanFam3.1.99.gtf-chr1-38X-CDS-forDepthOfCoverage.interval.list \
+# -O ${MuTect2_out}/${SampleName}_MuTect2_GATK4_noDBSNP.vcf
 
 
-## with PON
-java -Xmx32g -cp  $script/ DbSNP_filtering \
-${reference}/DbSNP_canFam3_version151-DogSD_Feb2020_V4.vcf \
-${MuTect2_out}/DbSNP_filtered-${SampleName}_MuTect2_GATK4.vcf \
-${MuTect2_out}/PON_DbSNP_filtered-${SampleName}_MuTect2_GATK4.vcf
+# ## pass this raw data to LearnReadOrientationModel:
+# gatk LearnReadOrientationModel -I ${MuTect2_out}/${SampleName}-f1r2.tar.gz -O ${MuTect2_out}/read-orientation-model.tar.gz
+
+# # filtered Mutect2 files
+
+# gatk FilterMutectCalls -R ${reference}/canFam3.fa \
+# o=-priors ${MuTect2_out}/read-orientation-model.tar.gz \
+# -V ${MuTect2_out}/${SampleName}_MuTect2_GATK4_noDBSNP.vcf \
+# O=${MuTect2_out}/filtered-${SampleName}_MuTect2_GATK4_noDBSNP.vcf
+
+# cd ${MuTect2_out}
+
+# ### filter Mutect2 #####
+
+# ## with DbSNP
+# java -Xmx32g -cp  $scripts/ DbSNP_filtering \
+# ${reference}/DbSNP_canFam3_version151-DogSD_Feb2020_V4.vcf \
+# ${MuTect2_out}/filtered-${SampleName}_MuTect2_GATK4_noDBSNP.vcf \
+# ${MuTect2_out}/DbSNP_filtered-${SampleName}_MuTect2_GATK4.vcf
 
 
-### Mutect2 5 steps filtering
+# ## with PON
+# java -Xmx32g -cp  $scripts/ DbSNP_filtering \
+# ${reference}/DbSNP_canFam3_version151-DogSD_Feb2020_V4.vcf \
+# ${MuTect2_out}/DbSNP_filtered-${SampleName}_MuTect2_GATK4.vcf \
+# ${MuTect2_out}/PON_DbSNP_filtered-${SampleName}_MuTect2_GATK4.vcf
 
-cd ${MuTect2_out}
 
-ml Anaconda3/2020.02
-#source activate py35
+# ### Mutect2 5 steps filtering
 
-#awk '$7 == "PASS" {print $0}' ${MuTect2_out}/PON_DbSNP_filtered-${SampleName}_MuTect2_GATK4.vcf > ${MuTect2_out}/PON_DbSNP_filtered-${SampleName}_MuTect2_GATK4.vcf-PASS
+# cd ${MuTect2_out}
 
-# 5 Steps filtering
-python $script/Mutect2_5Steps_filtering.py \
-${MuTect2_out}/PON_DbSNP_filtered-${SampleName}_MuTect2_GATK4.vcf \
-${MuTect2_out}/PON_DbSNP_filtered-${SampleName}_MuTect2_GATK4.vcf \
-${MuTect2_out}/${SampleName}_VAF_Before.txt \
-${MuTect2_out}/${SampleName}_VAF_After.txt \
-${MuTect2_out}/${SampleName}_whyout.txt \
-${SampleName}
+# ml Anaconda3/2020.02
+# #source activate py35
+
+# #awk '$7 == "PASS" {print $0}' ${MuTect2_out}/PON_DbSNP_filtered-${SampleName}_MuTect2_GATK4.vcf > ${MuTect2_out}/PON_DbSNP_filtered-${SampleName}_MuTect2_GATK4.vcf-PASS
+
+# # 5 Steps filtering
+# python $scripts/Mutect2_5Steps_filtering.py \
+# ${MuTect2_out}/PON_DbSNP_filtered-${SampleName}_MuTect2_GATK4.vcf \
+# ${MuTect2_out}/PON_DbSNP_filtered-${SampleName}_MuTect2_GATK4.vcf \
+# ${MuTect2_out}/${SampleName}_VAF_Before.txt \
+# ${MuTect2_out}/${SampleName}_VAF_After.txt \
+# ${MuTect2_out}/${SampleName}_whyout.txt \
+# ${SampleName}
 
 
 ## Strelka
@@ -258,7 +258,7 @@ module load SAMtools/1.9-GCC-8.3.0
 #samtools index ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.realigned.bam
 #samtools index ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.realigned.bam
 
-${script}/strelka-2.9.2.centos6_x86_64/bin/configureStrelkaSomaticWorkflow.py \
+${scripts}/strelka-2.9.2.centos6_x86_64/bin/configureStrelkaSomaticWorkflow.py \
 --normalBam ${results}/${Normal_Run}_rg_added_sorted_dedupped_removed.realigned.bam \
 --tumorBam ${results}/${Tumor_Run}_rg_added_sorted_dedupped_removed.realigned.bam \
 --referenceFasta ${reference}/canFam3.fa \
@@ -270,7 +270,7 @@ $strelka_out/runWorkflow.py -m local -j 20
 
 ## limit strelka result into CDS region
 
-python2 $script/Limit_vcf_to_CDS.py $strelka_out/results/variants/somatic.indels.vcf.gz $reference/Canis_familiaris.CanFam3.1.99.gtf-chr1-38X-CDS-forDepthOfCoverage.interval_list
+python2 $scripts/Limit_vcf_to_CDS.py $strelka_out/results/variants/somatic.indels.vcf.gz $reference/Canis_familiaris.CanFam3.1.99.gtf-chr1-38X-CDS-forDepthOfCoverage.interval_list
 
 
 ### Annovar for strelka
@@ -289,7 +289,7 @@ perl $reference/annovar_CanFam3.1.99.gtf/annotate_variation.pl --buildver canFam
 $reference/annovar_CanFam3.1.99.gtf
 
 # add gene name
-python2 $script/Add_GeneName_N_Signature.py $strelka_out/results/variants/somatic.indels.vcf_canFam3.1.99_CDS-PASS-avinput.exonic_variant_function \
+python2 $scripts/Add_GeneName_N_Signature.py $strelka_out/results/variants/somatic.indels.vcf_canFam3.1.99_CDS-PASS-avinput.exonic_variant_function \
 $reference/Canis_familiaris.CanFam3.1.99.chr.gtf_geneNamePair.txt
 
 # ####### remove unneeded files #######
